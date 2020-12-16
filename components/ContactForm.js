b778e313d90beb43b4c4cf9deeeaa4bdce248528
Sam <sam.montelongo@gmail.com>
2020-12-15 20:15:43 -0600

    Refactoring

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -2,52 +2,52 @@ import React from "react";
 
 export default function ContactForm() {
   return (
-    <div className="container-contact100">
-      <div className="wrap-contact100">
-        <form className="contact100-form validate-form">
+    <div className="container-contact">
+      <div className="wrap-contact">
+        <form className="contact-form validate-form">
           <div
-            className="wrap-input100 validate-input"
+            className="wrap-input validate-input"
             data-validate="Name is required"
           >
             <input
-              className="input100"
+              className="input"
               type="text"
               name="name"
               placeholder="Name"
             />
-            <span className="focus-input100-1"></span>
-            <span className="focus-input100-2"></span>
+            <span className="focus-input-1"></span>
+            <span className="focus-input-2"></span>
           </div>
 
           <div
-            className="wrap-input100 validate-input"
+            className="wrap-input validate-input"
             data-validate="Valid email is required: ex@abc.xyz"
           >
             <input
-              className="input100"
+              className="input"
               type="text"
               name="email"
               placeholder="Email"
             />
-            <span className="focus-input100-1"></span>
-            <span className="focus-input100-2"></span>
+            <span className="focus-input-1"></span>
+            <span className="focus-input-2"></span>
           </div>
 
           <div
-            className="wrap-input100 validate-input"
+            className="wrap-input validate-input"
             data-validate="Message is required"
           >
             <textarea
-              className="input100"
+              className="input"
               name="message"
               placeholder="Message"
             ></textarea>
-            <span className="focus-input100-1"></span>
-            <span className="focus-input100-2"></span>
+            <span className="focus-input-1"></span>
+            <span className="focus-input-2"></span>
           </div>
 
-          <div className="container-contact100-form-btn">
-            <button className="contact100-form-btn">Send</button>
+          <div className="container-contact-form-btn">
+            <button className="contact-form-btn">Send</button>
           </div>
         </form>
       </div>
