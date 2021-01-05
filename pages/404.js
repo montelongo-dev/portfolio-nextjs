a08842b9a8f4f8704133a6aeb281c94edc0c1ae3
Sam <sam.montelongo@gmail.com>
2021-01-04 20:52:29 -0600

    Adding a submission page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+export default function Error() {
+  return (
+    <div className="error">
+      <div>
+        <h1>404</h1>
+        <div className="test">
+          <h2>Page not found</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/contact.js
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+export default function Contact() {
+  return (
+    <div className="contact__submission">
+      <p>Thank you for reaching out! I will get back to you soon.</p>
+      <p>Sam Montelongo</p>
+      <p>
+        <FontAwesomeIcon
+          icon={faArrowLeft}
+          size="1x"
+          className="skills__resume-download"
+          color="#496d89"
+        />
+        <a href="/"> Back to the site</a>
+      </p>
+    </div>
+  );
+}
